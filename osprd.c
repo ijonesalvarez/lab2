c6f983fb217c371c4ce87a1f75702f0063c27a18
israel <israel-jones123@hotmail.com>
2014-05-07 14:49:47 -0700

    Authors added

--- osprd.c
+++ osprd.c
@@ -34,7 +34,7 @@
 MODULE_LICENSE("Dual BSD/GPL");
 MODULE_DESCRIPTION("CS 111 RAM Disk");
 // EXERCISE: Pass your names into the kernel as the module's authors.
-MODULE_AUTHOR("Skeletor");
+MODULE_AUTHOR("Israel Jones-Alvarez, Omar Leyva");
 
 #define OSPRD_MAJOR	222
 
