ff8dc615054470bc002486ac4f5ac7020fb8673c
israel <israel-jones123@hotmail.com>
2014-05-07 20:57:03 -0700

    opr done

--- osprd.c
+++ osprd.c
@@ -33,7 +33,6 @@
 
 MODULE_LICENSE("Dual BSD/GPL");
 MODULE_DESCRIPTION("CS 111 RAM Disk");
-// EXERCISE: Pass your names into the kernel as the module's authors.
 MODULE_AUTHOR("Israel Jones-Alvarez, Omar Leyva");
 
 #define OSPRD_MAJOR	222
@@ -121,8 +120,23 @@ static void osprd_process_request(osprd_info_t *d, struct request *req)
 	// 'req->buffer' members, and the rq_data_dir() function.
 
 	// Your code here.
-	eprintk("Should process request...\n");
+	
+        int size = req->current_nr_sector;
+        int offset = req->sector * SECTOR_SIZE
 
+        if (rq_data_dir(req) == READ)
+        {
+           memcpy(req->buffer, d->data + offset, size); 
+        }     
+        else if (re_data_dir(req) == WRITE)
+        {
+           memcpy(d->data + offset, req->buffer, size)
+        } 
+        else
+        {
+           end_request(req, 0);  
+        }
+ 
 	end_request(req, 1);
 }
 
